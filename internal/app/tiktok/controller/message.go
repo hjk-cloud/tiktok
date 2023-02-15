31deeabb0029c782d8edf1c30c7a7698b4508071
hummingg <xuhuiming6991@gmail.com>
2023-02-15 15:25:58 +0800

    message run

--- internal/app/tiktok/controller/message.go
+++ internal/app/tiktok/controller/message.go
@@ -11,9 +11,10 @@ import (
 	"github.com/hjk-cloud/tiktok/internal/pkg/model/vo"
 )
 
+// 保存到redis比较好
 var tempChat = map[string][]vo.Message{}
 
-var messageIdSequence = int64(1)
+var messageIdSequence = int64(0)
 
 type ChatResponse struct {
 	vo.Response
@@ -33,8 +34,15 @@ func MessageAction(c *gin.Context) {
 		atomic.AddInt64(&messageIdSequence, 1)
 		curMessage := vo.Message{
 			Id:         messageIdSequence,
+			FromUserId: user.Id,
+			ToUserId:   int64(userIdB),
 			Content:    content,
-			CreateTime: time.Now().Format(time.Kitchen),
+			CreateTime: time.Now().Local().Unix(),
+		}
+		if user.Id < int64(userIdB) {
+			curMessage.FirstRead = true
+		} else {
+			curMessage.SecondRead = true
 		}
 
 		if messages, exist := tempChat[chatKey]; exist {
@@ -52,30 +60,48 @@ func MessageAction(c *gin.Context) {
 func MessageChat(c *gin.Context) {
 	token := c.Query("token")
 	toUserId := c.Query("to_user_id")
-
-	if user, exist := UsersLoginInfo[token]; exist {
-		userIdB, _ := strconv.Atoi(toUserId)
-		chatKey := genChatKey(user.Id, int64(userIdB))
-
-		fmt.Printf("##### from %d to %d\n", user.Id, userIdB)
-		tempChat[chatKey] = []vo.Message{
-			vo.Message{
-				Id:         1,
-				Content:    "111",
-				CreateTime: time.Now().Format("2006-01-02 15:04:05"),
-			},
-			vo.Message{
-				Id:         1,
-				Content:    "222",
-				CreateTime: time.Now().Format("2006-01-02 15:04:05"),
-			},
-		}
-		fmt.Printf("##### %#v\n", tempChat)
-
-		c.JSON(http.StatusOK, ChatResponse{Response: vo.Response{StatusCode: 0}, MessageList: tempChat[chatKey]})
-	} else {
+	user, exist := UsersLoginInfo[token]
+	if !exist {
 		c.JSON(http.StatusOK, vo.Response{StatusCode: 1, StatusMsg: "User doesn't exist"})
 	}
+	userIdB, _ := strconv.Atoi(toUserId)
+	chatKey := genChatKey(user.Id, int64(userIdB))
+
+	fmt.Printf("##### from %d to %d\n", user.Id, userIdB)
+	messageList := []vo.Message{}
+	leftChat := []vo.Message{}
+	if user.Id < int64(userIdB) {
+		for _, msg := range tempChat[chatKey] {
+			if msg.FirstRead {
+				if !msg.SecondRead {
+					leftChat = append(leftChat, msg)
+				}
+				continue
+			}
+			msg.FirstRead = true
+			if !msg.SecondRead {
+				leftChat = append(leftChat, msg)
+			}
+			messageList = append(messageList, msg)
+		}
+	} else {
+		for _, msg := range tempChat[chatKey] {
+			if msg.SecondRead {
+				if !msg.FirstRead {
+					leftChat = append(leftChat, msg)
+				}
+				continue
+			}
+			msg.SecondRead = true
+			if !msg.FirstRead {
+				leftChat = append(leftChat, msg)
+			}
+			messageList = append(messageList, msg)
+		}
+	}
+	tempChat[chatKey] = leftChat
+	fmt.Printf("#####%#v\n", tempChat[chatKey])
+	c.JSON(http.StatusOK, ChatResponse{Response: vo.Response{StatusCode: 0}, MessageList: messageList})
 }
 
 func genChatKey(userIdA int64, userIdB int64) string {

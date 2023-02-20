c0e56476ba1588010389d0df76466677b0c0114d
hummingg <xuhuiming6991@gmail.com>
2023-02-21 00:06:14 +0800

    video: user visit author

--- internal/app/tiktok/service/publish_list.go
+++ internal/app/tiktok/service/publish_list.go
@@ -4,35 +4,46 @@ import (
 	"github.com/hjk-cloud/tiktok/internal/pkg/model/dto"
 	"github.com/hjk-cloud/tiktok/internal/pkg/model/vo"
 	repo "github.com/hjk-cloud/tiktok/internal/pkg/repository"
+	"github.com/hjk-cloud/tiktok/util"
 )
 
 // TODO
 func PublishList(dto *dto.PublishListDTO) ([]vo.Video, error) {
-	authorDo, _ := repo.NewUserInfoDaoInstance().QueryUserById(dto.UserId)
-	author := vo.User{
-		Id:            authorDo.Id,
-		Name:          authorDo.Name,
-		FollowCount:   authorDo.FollowCount,
-		FollowerCount: authorDo.FollowerCount,
-		IsFollow:      false,
+	authorDo, err := repo.NewUserInfoDaoInstance().QueryUserById(dto.UserId)
+	author := vo.User{}
+	if err == nil && authorDo != nil {
+		author = vo.User{
+			Id:            authorDo.Id,
+			Name:          authorDo.Name,
+			FollowCount:   authorDo.FollowCount,
+			FollowerCount: authorDo.FollowerCount,
+			// IsFollow:      false, // TODO: 暂时不提供，可能并没有影响
+		}
+	}
+	authorId, err := util.JWTAuth(dto.Token)
+	if err != nil {
+		return nil, err
 	}
 
-	dos, err := repo.NewVideoRepoInstance().QueryVideoByAuthorId(dto.UserId)
-	vos := make([]vo.Video, len(dos))
-	if err != nil {
-		return vos, nil
-	}
-	for i, do := range dos {
-		vos[i] = vo.Video{
-			Id:            do.Id,
-			Author:        author,
-			PlayUrl:       do.PlayUrl,
-			CoverUrl:      do.CoverUrl,
-			FavoriteCount: do.FavoriteCount,
-			CommentCount:  do.CommentCount,
-			IsFavorite:    false,
-			Title:         do.Title,
-		}
+	vos, err := repo.NewVideoRepoInstance().QueryVideoInProfile(authorId, dto.UserId)
+	// vos := make([]vo.Video, len(dos))
+	// if err != nil {
+	// 	return vos, nil
+	// }
+	// // video -> favorite
+	// // followDos, err := repo.NewFollowDaoInstance()
+	for i := range vos {
+		// vos[i] = vo.Video{
+		// 	Id:            do.Id,
+		// 	Author:        author,
+		// 	PlayUrl:       do.PlayUrl,
+		// 	CoverUrl:      do.CoverUrl,
+		// 	FavoriteCount: do.FavoriteCount,
+		// 	CommentCount:  do.CommentCount,
+		// 	IsFavorite:    false, // TODO
+		// 	Title:         do.Title,
+		// }
+		vos[i].Author = author
 	}
 	return vos, nil
 }
--- internal/pkg/repository/video.go
+++ internal/pkg/repository/video.go
@@ -2,13 +2,15 @@ package repository
 
 import (
 	"errors"
-	"github.com/hjk-cloud/tiktok/config"
 	"log"
 	"sync"
 	"time"
 
+	"github.com/hjk-cloud/tiktok/config"
+
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/hjk-cloud/tiktok/internal/pkg/model/do"
+	"github.com/hjk-cloud/tiktok/internal/pkg/model/vo"
 
 	//_ "github.com/spf13/viper" // 只执行init()
 	"gorm.io/gorm"
@@ -104,12 +106,40 @@ func (*VideoRepo) MQueryVideoByLastTime(latestTime time.Time) ([]do.VideoDO, err
 	return videos, nil
 }
 
-func (*VideoRepo) QueryVideoByAuthorId(userId int64) ([]do.VideoDO, error) {
-	videos := []do.VideoDO{}
-	var err error
-	err = Db.Where(do.VideoDO{AuthorId: userId}, map[string]interface{}{"Status": 0}).Find(&videos).Error
-	if err == gorm.ErrRecordNotFound {
-		return videos, nil
+// authorId -> video
+// userId -> favorite(video)
+
+// 用户访问自己主页
+// func (*VideoRepo) QueryVideoByAuthorId(authorId int64) ([]do.VideoDO, error) {
+// 	var videos []do.VideoDO
+// 	err := Db.Where(do.VideoDO{AuthorId: authorId}, map[string]interface{}{"Status": 0}).Find(&videos).Error
+// 	if err == gorm.ErrRecordNotFound {
+// 		return videos, nil
+// 	}
+// 	return videos, err
+// }
+
+// 用户访问作者(也可能是用户本人)主页
+func (*VideoRepo) QueryVideoInProfile(userId int64, authorId int64) ([]vo.Video, error) {
+	var videos []vo.Video
+	rows, err := Db.Raw(`
+	SELECT a.Id, a.play_url, a.cover_url, a.comment_count, a.favorite_count, a.title, 1-IF(b.is_deleted IS NULL, 1, b.is_deleted) AS is_follow 
+	FROM t_video a LEFT JOIN t_favorite b 
+	ON a.id=b.object_Id AND b.subject_id=? AND b.is_deleted=0 AND b.object_type='video'
+	WHERE a.author_id=? AND a.Status=0;`, userId, authorId).Rows()
+	if err != nil {
+		return nil, err
 	}
-	return videos, err
+	//.Scan(&videos).Error
+	for rows.Next() {
+		var video vo.Video
+		err = rows.Scan(&video.Id, &video.PlayUrl, &video.CoverUrl, &video.CommentCount, &video.FavoriteCount,
+			&video.Title, &video.IsFavorite)
+		if err == nil {
+			videos = append(videos, video)
+		} else {
+			log.Printf("##### %#v\n", err)
+		}
+	}
+	return videos, rows.Err()
 }
